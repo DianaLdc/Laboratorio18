7537c3ec551fc05fb6e4f1b928ebe87afac78935
Diana Rocio Ramos Rodrigo <dramosrod@unsa.edu.pe>
2025-11-28 18:39:30 -0500

    ejercicio 8

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
-document.getElementById("btnCargar").addEventListener("click", function() {
-    fetch("https://jsonplaceholder.typicode.com/users/2")
-        .then(res => res.json())
-        .then(usuario => {
-            const resultado = document.getElementById("resultado");
+document.getElementById("btnCargar").addEventListener("click", cargarUsuario);
 
-            resultado.innerHTML = `
-                <strong>Nombre:</strong> ${usuario.name} <br>
-                <strong>Email:</strong> ${usuario.email} <br>
-                <strong>Ciudad:</strong> ${usuario.address.city}
-            `;
-        })
-        .catch(error => console.error("Error:", error));
+async function cargarUsuario() {
+    try {
+        const respuesta = await fetch("https://jsonplaceholder.typicode.com/users/2");
+        const usuario = await respuesta.json();
 
-});
+        const resultado = document.getElementById("resultado");
+
+        resultado.innerHTML = `
+            <strong>Nombre:</strong> ${usuario.name} <br>
+            <strong>Email:</strong> ${usuario.email} <br>
+            <strong>Ciudad:</strong> ${usuario.address.city}
+        `;
+
+    } catch (error) {
+        console.error("Error:", error);
+    }
+}
