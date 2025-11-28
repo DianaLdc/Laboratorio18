e90635ff1456062a378901fd380daa3103f8d274
Diana Rocio Ramos Rodrigo <dramosrod@unsa.edu.pe>
2025-11-28 18:36:17 -0500

    ejercicio 7

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
- async function mostrarNombres() {
-    try {
-        const respuesta = await fetch("https://jsonplaceholder.typicode.com/users");
-        const usuarios = await respuesta.json();
+document.getElementById("btnCargar").addEventListener("click", function() {
+    fetch("https://jsonplaceholder.typicode.com/users/2")
+        .then(res => res.json())
+        .then(usuario => {
+            const resultado = document.getElementById("resultado");
 
-        console.log("Nombres de los usuarios (async/await):");
+            resultado.innerHTML = `
+                <strong>Nombre:</strong> ${usuario.name} <br>
+                <strong>Email:</strong> ${usuario.email} <br>
+                <strong>Ciudad:</strong> ${usuario.address.city}
+            `;
+        })
+        .catch(error => console.error("Error:", error));
 
-        usuarios.forEach(usuario => {
-            console.log(usuario.name);
-        });
-
-    } catch (error) {
-        console.error("Error:", error);
-    }
-}
-
-mostrarNombres();
+});
