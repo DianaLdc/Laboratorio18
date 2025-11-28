72f9436ad1921552d8b5e845fce7aa14e62d1c48
Diana Rocio Ramos Rodrigo <dramosrod@unsa.edu.pe>
2025-11-28 18:18:11 -0500

    ejercicio 3

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
+fetch("https://jsonplaceholder.typicode.com/users/10")
+    .then(respuesta => respuesta.json())
+    .then(datos => {
+        console.log("Usuario con ID 10:");
+        console.log(datos);
+    })
+    .catch(error => console.error("Error:", error));
 
